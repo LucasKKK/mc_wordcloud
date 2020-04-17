0e094d6764a03e688e5a55e94ffce9f729a67cf4
Lucas <2443927272@qq.com>
2020-04-17 13:21:00 +0800

    none

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name = "mc_wordcloud",
     version = "0.0.1",
     author = "魔扣少儿编程",
-    author_email = "UNKNOW",
+    author_email = "2443927272@qq.com",
     url = "https://github.com/LucasKKK/mc_wordcloud",
     packages = setuptools.find_packages(),
     #include_package_date = True,
