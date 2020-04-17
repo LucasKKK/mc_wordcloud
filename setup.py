ec4ac6d13e8c197c5f749237328016c056be4702
Lucas <2443927272@qq.com>
2020-04-17 15:52:43 +0800

    none

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setuptools.setup(
     author = "魔扣少儿编程",
     author_email = "2443927272@qq.com",
     url = "https://github.com/LucasKKK/mc_wordcloud", 
-    data_files = [('mc_word', [
-        'mc_word/鲁班.jpg',
-        'mc_word/鲁班.txt'
+    data_files = [('mc_wordcloud', [
+        'mc_wordcloud/鲁班.jpg',
+        'mc_wordcloud/鲁班.txt'
     ])
     ],
-    packages = ['mc_word'],     #多个文件夹手动添加
+    packages = ['mc_wordcloud'],     #多个文件夹手动添加
     include_package_data = True,
     #packages = setuptools.find_packages(),
     install_requires = ['wordcloud', 'numpy', 'matplotlib', 'pillow']
