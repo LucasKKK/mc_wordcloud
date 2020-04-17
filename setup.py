9c1ca723159b5cab493a58389b25064820da7903
Lucas <2443927272@qq.com>
2020-04-17 10:49:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name = "mc_wordcloud",
     version = "0.0.1",
     author = "魔扣少儿编程",
-    url = "https://github.com/LucasKKK/mc_wordcloud"
-    include_package_date = True,
+    author_email = "UNKNOW",
+    url = "https://github.com/LucasKKK/mc_wordcloud",
+    packages = setuptools.find_packages(),
+    #include_package_date = True,
     install_requires = ['wordcloud', 'numpy', 'matplotlib', 'pillow']
 )
