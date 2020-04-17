9edc5ad60a07e7759e9da772bac49de298d1596e
Lucas <2443927272@qq.com>
2020-04-17 21:13:56 +0800

    tested

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 import setuptools
 setuptools.setup(
     name = "mc_wordcloud",
-    version = "0.0.8",
+    version = "0.0.9",
     author = "魔扣少儿编程",
     author_email = "2443927272@qq.com",
     url = "https://github.com/LucasKKK/mc_wordcloud", 
     data_files = [('mc_wordcloud', [
-        'mc_wordcloud/百里守约.jpg',
-        'mc_wordcloud/百里守约.txt',
-        #'mc_wordcloud/程咬金.jpg',  x
-        #'mc_wordcloud/程咬金.txt',
-        #'mc_wordcloud/狄仁杰.jpg',  x
-        #'mc_wordcloud/狄仁杰.txt',
-        'mc_wordcloud/貂蝉.jpg',
-        'mc_wordcloud/貂蝉.txt',
-        #'mc_wordcloud/宫本武藏.jpg',x
-        #'mc_wordcloud/宫本武藏.txt',
-        'mc_wordcloud/韩信.jpg',
-        'mc_wordcloud/韩信.txt',
-        #'mc_wordcloud/花木兰.jpg', x
-        #'mc_wordcloud/花木兰.txt',
-        'mc_wordcloud/李白.jpg',
-        'mc_wordcloud/李白.txt',
-        'mc_wordcloud/鲁班.jpg',
-        'mc_wordcloud/鲁班.txt',
-        'mc_wordcloud/上官婉儿.jpg',
-        'mc_wordcloud/上官婉儿.txt'
-        #'mc_wordcloud/孙悟空.jpg',x
-        #'mc_wordcloud/孙悟空.txt'
+        'mc_wordcloud/1.jpg',
+        'mc_wordcloud/1.txt',
+        'mc_wordcloud/2.jpg',
+        'mc_wordcloud/2.txt',
+        'mc_wordcloud/3.jpg',
+        'mc_wordcloud/3.txt',
+        'mc_wordcloud/4.jpg',
+        'mc_wordcloud/4.txt',
+        'mc_wordcloud/5.jpg',
+        'mc_wordcloud/5.txt',
+        'mc_wordcloud/6.jpg',
+        'mc_wordcloud/6.txt',
+        'mc_wordcloud/7.jpg',
+        'mc_wordcloud/7.txt',
+        'mc_wordcloud/8.jpg',
+        'mc_wordcloud/8.txt',
+        'mc_wordcloud/9.jpg',
+        'mc_wordcloud/9.txt',
+        'mc_wordcloud/10.jpg',
+        'mc_wordcloud/10.txt',
+        'mc_wordcloud/11.jpg',
+        'mc_wordcloud/11.txt'
     ])
     ],
     packages = ['mc_wordcloud'],     #多个文件夹手动添加
