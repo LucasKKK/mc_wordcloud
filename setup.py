9bbe60d3d00893e86c291463864624c627a2f5d5
Lucas <2443927272@qq.com>
2020-04-29 16:35:45 +0800

    none

--- setup.py
+++ setup.py
@@ -1,37 +1,21 @@
 import setuptools
 setuptools.setup(
     name = "mc_wordcloud",
-    version = "0.0.9",
+    version = "0.1.0",
     author = "魔扣少儿编程",
     author_email = "2443927272@qq.com",
     url = "https://github.com/LucasKKK/mc_wordcloud", 
     data_files = [('mc_wordcloud', [
-        'mc_wordcloud/1.jpg',
-        'mc_wordcloud/1.txt',
-        'mc_wordcloud/2.jpg',
-        'mc_wordcloud/2.txt',
-        'mc_wordcloud/3.jpg',
-        'mc_wordcloud/3.txt',
-        'mc_wordcloud/4.jpg',
-        'mc_wordcloud/4.txt',
-        'mc_wordcloud/5.jpg',
-        'mc_wordcloud/5.txt',
-        'mc_wordcloud/6.jpg',
-        'mc_wordcloud/6.txt',
-        'mc_wordcloud/7.jpg',
-        'mc_wordcloud/7.txt',
-        'mc_wordcloud/8.jpg',
-        'mc_wordcloud/8.txt',
-        'mc_wordcloud/9.jpg',
-        'mc_wordcloud/9.txt',
-        'mc_wordcloud/10.jpg',
-        'mc_wordcloud/10.txt',
-        'mc_wordcloud/11.jpg',
-        'mc_wordcloud/11.txt'
+        'mc_wordcloud/weapon_list.txt',
+        'mc_wordcloud/hero_name.txt',
+        'mc_wordcloud/tool_list.txt'
     ])
     ],
     packages = ['mc_wordcloud'],     #多个文件夹手动添加
     include_package_data = True,
     #packages = setuptools.find_packages(),
-    install_requires = ['wordcloud', 'numpy', 'matplotlib', 'pillow']
+
+    install_requires = ['wordcloud', 'matplotlib'],
+    zip_safe = False
+
 )
